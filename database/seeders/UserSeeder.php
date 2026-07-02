74b8a3505b81af96e941996af17a9fa467929b47
Mitesh <72382861+mitesh-ko@users.noreply.github.com>
2026-07-02 22:51:34 +0530

    User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $password = 'pa$$Word';
+
+        for ($i = 1; $i <= 10; $i++) {
+            User::firstOrCreate(
+                ['email' => "user{$i}@throtik.com"],
+                [
+                    'name' => "User {$i}",
+                    'password' => Hash::make($password),
+                ]
+            );
+        }
+    }
+}
